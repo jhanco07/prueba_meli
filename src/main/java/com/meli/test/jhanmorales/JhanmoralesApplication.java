dae18fe12af64e479d89ff1c9b89a0cd0201f24d
Jhan Morales <jhanco07@hotmail.com>
2022-05-29 18:17:13 -0500

    Initial commit

---
+++ src/main/java/com/meli/test/jhanmorales/JhanmoralesApplication.java
@@ -0,0 +1,13 @@
+package com.meli.test.jhanmorales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhanmoralesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JhanmoralesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/meli/test/jhanmorales/JhanmoralesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.test.jhanmorales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhanmoralesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
